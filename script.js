910ec9f108bf8ca809cae4ea917e47f3d9ba67be
Pedro Couto <pedrocouto2001@hotmail.com>
2023-01-01 19:37:40 +0000

    new e simulate

--- script.js
+++ script.js
@@ -30,17 +30,16 @@ function renderPlaces(places) {
 		let longitude = place.location.lng;
 
 		let model = document.createElement("a-entity");
-		model.setAttribute("position", {
-			x: 0,
-			y: -5,
-			z: 0,
-		});
+		// model.setAttribute("position", {
+		// 	x: 0,
+		// 	y: -5,
+		// 	z: 0,
+		// });
 		model.setAttribute(
-			"gps-entity-place",
+			"gps-new-entity-place",
 			`latitude: ${latitude}; longitude: ${longitude};`
 		);
 		model.setAttribute("gltf-model", "./assets/asset.gltf");
-		model.setAttribute("rotation", "0 180 0");
 		model.setAttribute("scale", "10 10 10");
 
 		scene.appendChild(model);
