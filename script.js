9dc7a291312887d4a88aeeff30882ebd7f689aa8
Pedro Couto <pedrocouto2001@hotmail.com>
2023-01-12 17:55:41 +0000

    topo

--- script.js
+++ script.js
@@ -1,3 +1,102 @@
+//websocket client
+var ws = new WebSocket("ws://172.22.21.135:3306");
+//websocket connection
+ws.onopen = function () {
+	console.log("Connected to server");
+};
+//websocket error
+ws.onerror = function (err) {
+	console.log("Error: " + err);
+};
+//websocket message
+ws.onmessage = function (event) {
+	console.log("Message: " + event.data);
+	loadApples(event.data);
+};
+//websocket close
+ws.onclose = function () {
+	console.log("Disconnected");
+};
+
+function loadApples(json) {
+	let scene = document.querySelector("a-scene");
+
+	// do {
+	// 	var element = scene.getObjectByName("fruit");
+	// 	console.log(element);
+	// 	scene.remove(element);
+	// } while (element);
+
+	// console.log(json);
+
+	json = json.replaceAll("'", '"');
+	const obj = JSON.parse(json);
+
+	var fruit = obj.mario.map;
+
+	// console.log("Fruit: ", fruit);
+
+	for (var key of Object.keys(fruit)) {
+		fruit[key].forEach((element) => {
+			//place element close to arvore coordinates
+			let model = document.createElement("a-entity");
+			model.setAttribute("material", `color: red`);
+			model.setAttribute("geometry", `primitive: sphere; radius: 1`);
+			model.setAttribute(
+				"gps-new-entity-place",
+				`latitude: ${39.734168 + (element.lat - 0.0001) / 10000 - 0.000075}; longitude: ${
+					-8.821121 + element.lon / 10000 - 0.00004
+				};`
+			);
+			model.setAttribute("scale", "0.1 0.1 0.1");
+			model.setAttribute("position", `0 ${2 + element.alt * 10} 0`);
+			model.setAttribute("clicker", null);
+
+			scene.appendChild(model);
+		});
+	}
+
+	AFRAME.registerComponent("clicker", {
+		init: function () {
+			this.el.addEventListener("click", (e) => {
+				let lat = e.srcElement.components["gps-new-entity-place"].attrValue.latitude;
+				let lon = e.srcElement.components["gps-new-entity-place"].attrValue.longitude;
+				let alt = e.srcElement.components["position"].data.y;
+				let model = document.createElement("a-text");
+
+				let x = 39.734167 / 1000000 + parseFloat(lat);
+				let y = -8.821121 / 1000000 + parseFloat(lon);
+				console.log(`lat:${lat}, lon:${lon}`);
+				console.log(`x:${x}, y:${y}`);
+
+				model.setAttribute("value", `X: ${lat}\nY: ${lon}\nZ: ${alt};`);
+
+				model.setAttribute(
+					"gps-new-entity-place",
+					`latitude: ${lat}; longitude: ${lon};`
+				);
+				model.setAttribute("position", `0 ${alt + 0.2} 0`);
+				model.setAttribute("scale", "1 1 1");
+				model.setAttribute("look-at", "[gps-new-camera]");
+				model.setAttribute("color", "black");
+				model.setAttribute("clickerremove", null);
+				scene.appendChild(model);
+			});
+		},
+	});
+
+	AFRAME.registerComponent("clickerremove", {
+		init: function () {
+			this.el.addEventListener("click", (e) => {
+				console.log("Click");
+				scene.removeChild(e.srcElement);
+			});
+		},
+	});
+
+	// console.log(scene)
+}
+
 window.onload = () => {
 	let places = staticLoadPlaces();
 	renderPlaces(places);
@@ -10,13 +109,6 @@ function staticLoadPlaces() {
 			location: {
 				lat: 39.734167,
 				lng: -8.821121,
-			},
-		},
-		{
-			name: "Arvore2",
-			location: {
-				lat: 39.733845,
-				lng: -8.820798,
 			},
 		},
 	];
@@ -36,11 +128,12 @@ function renderPlaces(places) {
 		// 	z: 0,
 		// });
 		model.setAttribute(
-			"gps-entity-place",
+			"gps-new-entity-place",
 			`latitude: ${latitude}; longitude: ${longitude};`
 		);
 		model.setAttribute("gltf-model", "./assets/asset.gltf");
 		model.setAttribute("scale", "10 10 10");
+		model.setAttribute("position", `0 -2.5 0`);
 
 		scene.appendChild(model);
 	});
